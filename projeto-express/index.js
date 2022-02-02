45c847cbc3aa6d646b036c27b6e3a6c61c025551
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-02-02 16:43:12 -0300

    Alterações no Index e Gitignore

--- projeto-express/index.js
+++ projeto-express/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+var usuarios = [];
+
 app.get('',(req , res) => {
     res.send('Hello World')
 })
@@ -18,6 +22,11 @@ app.post('' ,(req, res) => {
 app.post('/usuarios' ,(req, res) => {
     console.log(req)
     res.send('Cruzeiro o Maior de Minas!!!')
+})
+
+app.post('/usuarios', function (req,res){
+    usuarios.push(req.body);
+    req.send(200, usuarios);
 })
 
 app.listen(3000,() => {
