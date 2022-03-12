c772120129ed4744778bbacc4a9b034ec213d7c9
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-03-12 15:09:10 -0300

    Update httpNoNode.js

--- Módula04HTMLCSS/httpNoNode.js
+++ Módula04HTMLCSS/httpNoNode.js
@@ -1,7 +1,22 @@
 const http = require('http');
 
+/*http.createServer((req,req) => {
+  res.writeHead(200 , {"Content-Type": "text/plain"});
+  res.end("Meu primeiro servidor!");
+}).listen(3030)*/
+
+
 http.createServer((req,req) => {
   res.writeHead(200 , {"Content-Type": "text/plain"});
+
+    switch(req.url){
+      case"/":
+        res.end("Você está na página home!")
+          break;
+      case"/contato":
+        res.end("Você está na página contato!")
+          break;
+    }
   res.end("Meu primeiro servidor!");
 }).listen(3030)
 
